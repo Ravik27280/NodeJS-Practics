cf9ee6d957fcb56b1b7b1fd14e9786cf83143b15
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2022-10-01 23:18:42 +0530

    file

--- index.js
+++ index.js
@@ -7,4 +7,18 @@
 // resp.end();     
 // }).listen(5000);
 
-console.log(process.argv);
+
+// to enter user aregument
+// console.log(process.argv);
+
+const fs =require('fs');
+const input= process.argv;
+if(input[2]=='add'){
+    fs.writeFileSync(input[3],input[4])
+}
+else if(input[2]=='remove'){
+    fs.unlinkSync(input[3])
+}
+else{
+    console.log('Invalid input')
+}
