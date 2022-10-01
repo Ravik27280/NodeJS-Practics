1dd3621bf6737414060c12e3792383c3b64878b7
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2022-10-01 21:23:48 +0530

    firsrt commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const data=[
+    {name:'Ravi kumar',email:'Ravik27280@gmail.com'},
+    {name:'Ram',email:'Ram27280@gmail.com'},
+    {name:'Rahul',email:'Rahul27280@gmail.com'},
+    {name:'Rohan',email:'Rohan27280@gmail.com'}
+]
+module.exports=data;
